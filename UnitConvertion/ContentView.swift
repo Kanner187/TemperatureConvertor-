857a3d1ddbf22b40538205e499847bbc2e7be595
Levit Osei Kanner <lkanner21@gmail.com>
2019-10-27 17:58:25 +0000

    Initial commit

--- UnitConvertion/ContentView.swift
+++ UnitConvertion/ContentView.swift
@@ -9,8 +9,87 @@
 import SwiftUI
 
 struct ContentView: View {
+    //Properties
+    @State private var figure = ""
+    @State private var unitFrom = 0
+    @State private var unitTo = 0
+    
+    private var units = ["Celsius" , "Fahrenheit" , "Kelvin"]
+    
+    private var convertedValue: Double {
+        //Do computations here...
+        let inputValue = Double(figure) ?? 0
+        
+        //Conversion from Celsius
+        if unitFrom == 0 {
+            //To Fahrenheit
+            if unitTo == 1{
+                return Double(inputValue * (9/5) + 32)
+            }
+            //To Kelvin
+            else if unitTo == 2 {
+                return Double(inputValue + 273.15)
+            }
+        }
+        
+        //Conversion from Fahrenheit
+        if unitFrom == 1{
+            //To Celsius
+            if unitTo == 0 {
+                return 3
+            }
+            //To Kelvin
+            else if unitTo == 2 {
+                return 4
+            }
+        }
+        
+        //Conversion from Kelvin
+        if unitFrom == 2{
+            //To Celsius
+            if unitTo == 0{
+                return 5
+            }
+            //To Kelvin
+            else if unitTo == 1 {
+                return 6
+            }
+        }
+        
+        return inputValue
+    }
+    
+    //Body Initialization
     var body: some View {
-        Text("Hello World")
+        NavigationView{
+            Form{
+                TextField("Digit", text: $figure)
+                
+                Section(header: Text("Unit From")) {
+                    Picker("from", selection: $unitFrom) {
+                        ForEach(0..<units.count){
+                            Text("\(self.units[$0])")
+                        }
+                    }
+                .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                
+                Section(header: Text("Unit To:")) {
+                    Picker("To", selection: $unitTo) {
+                        ForEach(0..<units.count) {
+                            Text("\(self.units[$0])")
+                        }
+                    }
+                .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section(header: Text("Converted Value")){
+                    Text("\(self.convertedValue , specifier: "%.2f")")
+                }
+            }
+        .navigationBarTitle("Convert")
+        }
     }
 }
 
